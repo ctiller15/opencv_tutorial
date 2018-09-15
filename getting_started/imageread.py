ce191bfc29fc1e5c3fcbc5790d5dfe1e8801c2d1
ctiller15 <christiller2015@gmail.com>
2018-09-15 00:26:59 -0400

    Learn to read images

---
+++ getting_started/imageread.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+# load a color image in grayscale
+img = cv2.imread('../images/Stanley_Camo.jpg', 0)
+#print(img)
+#Resize
+# imresize = cv2.resize(img, (800, 640))
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
